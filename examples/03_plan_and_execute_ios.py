1f906545b4678e35b5c32f017a65b341990fff10
mahsumdemir <mahsum.demir@wandelbots.com>
2024-12-03 12:03:06 +0100

    ideas from christoph

--- examples/03_plan_and_execute_ios.py
+++ examples/03_plan_and_execute_ios.py
@@ -11,12 +11,13 @@ import numpy as np
 async def main():
     nova = Nova()
     cell = nova.cell()
+    controller = await cell.controller("ur")
 
     # Define a home position
     home_joints = (0, -np.pi / 2, -np.pi / 2, -np.pi / 2, np.pi / 2, 0)
 
     # Connect to the controller and activate motion groups
-    async with await cell.controller("ur") as controller:
+    async with controller:
         motion_group = controller.get_motion_group()
 
         # Get current TCP pose and offset it slightly along the x-axis
@@ -25,11 +26,14 @@ async def main():
         trajectory = MotionTrajectory(
             items=[
                 jnt(home_joints),
+                controller.write_on_path("digital_out[0]", value=False),
                 WriteAction(device_id="ur", key="digital_out[0]", value=False),
                 ptp(target_pose),
                 jnt(home_joints),
             ]
         )
+
+        io_value = await controller.read_io("digital_out[0]")
 
         # plan_response = await motion_group.plan(trajectory, tcp="Flange")
         # print(plan_response)
