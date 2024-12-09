4e4119053b77ca620805122b52d5a2b374675422
mahsumdemir <mahsum.demir@wandelbots.com>
2024-12-09 16:26:44 +0100

    change the order of the functions

--- examples/04_move_multiple_robots.py
+++ examples/04_move_multiple_robots.py
@@ -3,15 +3,6 @@ import asyncio
 import numpy as np
 
 
-async def main():
-    nova = Nova()
-    cell = nova.cell()
-    ur = await cell.controller("ur")
-    kuka = await cell.controller("kuka")
-
-    await asyncio.gather(move_robot(ur), move_robot(kuka))
-
-
 async def move_robot(controller: Controller):
     home_joints = (0, -np.pi / 4, -np.pi / 4, -np.pi / 4, np.pi / 4, 0)
 
@@ -25,6 +16,15 @@ async def move_robot(controller: Controller):
         await motion_group.run(
             actions, tcp="Flange", movement_controller=speed_up_movement_controller
         )
+
+
+async def main():
+    nova = Nova()
+    cell = nova.cell()
+    ur = await cell.controller("ur")
+    kuka = await cell.controller("kuka")
+
+    await asyncio.gather(move_robot(ur), move_robot(kuka))
 
 
 if __name__ == "__main__":
