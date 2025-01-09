783ac0fdf7f2c38ac15a4cb0721fe7460e63192b
mahsumdemir <mahsum.demir@wandelbots.com>
2025-01-09 17:18:26 +0100

    add log level and remove logs

--- nova/core/logging_setup.py
+++ nova/core/logging_setup.py
@@ -3,12 +3,11 @@ from loguru import logger
 
 
 def configure_logging(log_level: str = "INFO"):
-    # Remove any existing handlers (including default ones)
     logger.remove()
-
-    # Add your console handler (you can also add file handlers here)
     logger.add(
-        sys.stderr, level=log_level, format="<green>{time}</green> | <level>{message}</level>"
+        sys.stderr,
+        level=log_level,
+        format="<green>{time}</green> | <level>{level}</level> | <level>{message}</level>"
     )
 
     return logger
