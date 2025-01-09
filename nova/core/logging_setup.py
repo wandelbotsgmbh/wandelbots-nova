622102d278993e2a63f60abc9f6fe1dcd01a9072
mahsumdemir <mahsum.demir@wandelbots.com>
2025-01-09 17:19:21 +0100

    ruff format

--- nova/core/logging_setup.py
+++ nova/core/logging_setup.py
@@ -7,7 +7,7 @@ def configure_logging(log_level: str = "INFO"):
     logger.add(
         sys.stderr,
         level=log_level,
-        format="<green>{time}</green> | <level>{level}</level> | <level>{message}</level>"
+        format="<green>{time}</green> | <level>{level}</level> | <level>{message}</level>",
     )
 
     return logger
