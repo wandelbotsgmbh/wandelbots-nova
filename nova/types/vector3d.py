59e813c71ce191f67c266e8887dbffeebf819d62
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2025-01-09 15:51:37 +0100

    feat: Extend `Vector3d`
    Give it capabilities that formerly was split between `Orientation` and
    `Position` types. We decided to consolidate those into Vector3d because,
    technically, all of them are just 3-vectors. The semantic
    differentiation may be nice on paper but turned out to be unnecessarily
    complex and all-over-the-place. Instead, have one powerful vector3d type
    for all of them.
    
    Also allow for some new, common functions like negation and substractions.
    
    Don't add this geometricalgebra stuff and related to it, though.

--- nova/types/vector3d.py
+++ nova/types/vector3d.py
@@ -1,6 +1,8 @@
-import wandelbots_api_client as wb
 from typing import Any
+
+import numpy as np
 import pydantic
+import wandelbots_api_client as wb
 
 
 class Vector3d(pydantic.BaseModel):
@@ -14,16 +16,33 @@ class Vector3d(pydantic.BaseModel):
     y: float | int
     z: float | int
 
-    def __add__(self, other: Any):
-        raise TypeError()
+    def __neg__(self) -> "Vector3d":
+        return type(self)(x=-self.x, y=-self.y, z=-self.z)
 
-    def __radd__(self, other: Any):
-        raise TypeError()
+    def __add__(self, other: Any) -> "Vector3d":
+        if isinstance(other, (float, int)):
+            return type(self)(x=self.x + other, y=self.y + other, z=self.z + other)
+        if isinstance(other, Vector3d):
+            return type(self)(x=self.x + other.x, y=self.y + other.y, z=self.z + other.z)
+        return NotImplemented
+
+    def __radd__(self, other: Any) -> "Vector3d":
+        return self.__add__(other)
+
+    def __sub__(self, other: Any) -> "Vector3d":
+        return self.__add__(-other)
+
+    def __rsub__(self, other: Any) -> "Vector3d":
+        if isinstance(other, (float, int)):
+            return type(self)(x=other - self.x, y=other - self.y, z=other - self.z)
+        if isinstance(other, Vector3d):
+            return type(self)(x=other.x - self.x, y=other.y - self.y, z=other.z - self.z)
+        return NotImplemented
 
     def __mul__(self, other: Any) -> "Vector3d":
-        if not isinstance(other, (float, int)):
-            return NotImplemented
-        return type(self)(x=other * self.x, y=other * self.y, z=other * self.z)
+        if isinstance(other, (float, int)):
+            return type(self)(x=other * self.x, y=other * self.y, z=other * self.z)
+        return NotImplemented
 
     def __rmul__(self, other: Any) -> "Vector3d":
         return self * other
@@ -58,6 +77,12 @@ class Vector3d(pydantic.BaseModel):
         (10.0, 20.5, 30.2)
         """
         return self.x, self.y, self.z
+
+    def to_quaternion(self):
+        """Interpret the object as a rotation vector and convert it to a quaternion"""
+        values = np.asarray(self)
+        half_angle = np.linalg.norm(values) / 2
+        return np.concatenate([np.cos(half_angle)[None], values * np.sinc(half_angle / np.pi) / 2])
 
     @pydantic.model_serializer
     def serialize_model(self) -> wb.models.Vector3d:
