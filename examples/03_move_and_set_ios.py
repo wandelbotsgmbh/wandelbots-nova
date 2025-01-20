7153968dc968a5cf80d608abcb1dd2c028435cd4
cbiering <christoph.biering@wandelbots.com>
2025-01-20 12:14:34 +0100

    fix

--- examples/03_move_and_set_ios.py
+++ examples/03_move_and_set_ios.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from nova import Nova
+from nova.api import models
 
 # TODO: update I/O interaction interface
 from nova.actions import WriteAction, jnt, ptp
@@ -16,16 +17,19 @@ Prerequisites:
 
 
 async def main():
-    nova = Nova()
-    cell = nova.cell()
-    controllers = await cell.controllers()
-    controller = controllers[0]
+    with Nova() as nova:
+        cell = nova.cell()
+        controller = await cell.ensure_virtual_robot_controller(
+            "ur",
+            models.VirtualControllerTypes.UNIVERSALROBOTS_MINUS_UR10E,
+            models.Manufacturer.UNIVERSALROBOTS,
+        )
 
-    # Connect to the controller and activate motion groups
-    async with controller[0] as motion_group:
-        home_joints = await motion_group.joints()
-        tcp_names = await motion_group.tcp_names()
-        tcp = tcp_names[0]
+        # Connect to the controller and activate motion groups
+        async with controller[0] as motion_group:
+            home_joints = await motion_group.joints()
+            tcp_names = await motion_group.tcp_names()
+            tcp = tcp_names[0]
 
         # Get current TCP pose and offset it slightly along the x-axis
         current_pose = await motion_group.tcp_pose(tcp)
@@ -37,10 +41,8 @@ async def main():
             jnt(home_joints),
         ]
 
-        # io_value = await controller.read_io("digital_out[0]")
-        await motion_group.plan_and_execute(actions, tcp)
-
-    await nova.close()
+            # io_value = await controller.read_io("digital_out[0]")
+            await motion_group.plan_and_execute(actions, tcp)
 
 
 if __name__ == "__main__":
