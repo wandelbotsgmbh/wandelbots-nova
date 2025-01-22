977fe075e830334c37443de06f7ba93265a7ca5d
Stefan Wagner <stefan.wagner@wandelbots.com>
2025-01-22 11:50:19 +0100

    feat: expose error object in exception

--- nova/core/exceptions.py
+++ nova/core/exceptions.py
@@ -10,14 +10,31 @@ class ControllerNotFoundException(Exception):
 
 class PlanTrajectoryFailed(Exception):
     def __init__(self, error: wb.models.PlanTrajectoryFailedResponse):
+        self._error = error
         super().__init__(f"Plan trajectory failed: {json.dumps(error.to_dict(), indent=2)}")
 
+    @property
+    def error(self) -> wb.models.PlanTrajectoryFailedResponse:
+        """Return the original PlanTrajectoryFailedResponse object."""
+        return self._error
 
 class InitMovementFailed(Exception):
     def __init__(self, error: wb.models.InitializeMovementResponseInitResponse):
+        self._error = error
         super().__init__(f"Initial movement failed: {json.dumps(error.to_dict(), indent=2)}")
+
+    @property
+    def error(self) -> wb.models.InitializeMovementResponseInitResponse:
+        """Return the original InitializeMovementResponseInitResponse object."""
+        return self._error
 
 
 class LoadPlanFailed(Exception):
     def __init__(self, error: wb.models.PlanSuccessfulResponse):
+        self._error = error
         super().__init__(f"Load plan failed: {json.dumps(error.to_dict(), indent=2)}")
+
+    @property
+    def error(self) -> wb.models.PlanSuccessfulResponse:
+        """Return the original PlanSuccessfulResponse object."""
+        return self._error
