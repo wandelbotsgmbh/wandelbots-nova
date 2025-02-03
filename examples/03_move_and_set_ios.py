7f8dfccd36c25d009edd865c2405c4c527c141a7
mahsumdemir <mahsum.demir@wandelbots.com>
2025-02-03 15:28:30 +0100

    delete outdated comment

--- examples/03_move_and_set_ios.py
+++ examples/03_move_and_set_ios.py
@@ -42,7 +42,6 @@ async def main():
                 jnt(home_joints),
             ]
 
-            # io_value = await controller.read_io("digital_out[0]")
             await motion_group.plan_and_execute(actions, tcp)
 
         await cell.delete_robot_controller(controller.name)
