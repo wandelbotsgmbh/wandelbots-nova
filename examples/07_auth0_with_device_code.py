2e0dae6b1c2ddb56ff66cbcf4a3033165a414a55
Lars Beier <sn3ek@users.noreply.github.com>
2025-02-04 13:59:41 +0100

    feat: Make authorization flow asynchron. (#34)
    Co-authored-by: mahsumdemir <mahsum.demir@wandelbots.com>

--- examples/07_auth0_with_device_code.py
+++ examples/07_auth0_with_device_code.py
@@ -1,4 +1,5 @@
 from nova.auth.authorization import Auth0DeviceAuthorization
+import asyncio
 
 """
 Example: Perform device authorization with Auth0.
@@ -8,14 +9,14 @@ Prerequisites:
 """
 
 
-def main():
+async def main():
     # Replace these values with your Auth0 domain, client ID, and audience
-    auth0_domain = "YOUR-AUTH0-DOMAIN"
-    auth0_client_id = "YOUR-AUTH0-CLIENT-ID"
-    auth0_audience = "YOUR-AUTH0-AUDIENCE"
+    AUTH0_DOMAIN = "YOUR-AUTH0-DOMAIN"
+    AUTH0_CLIENT_ID = "YOUR-AUTH0-CLIENT-ID"
+    AUTH0_AUDIENCE = "YOUR-AUTH0-AUDIENCE"
 
     # Initialize the Auth0DeviceAuthorization instance
-    auth0_device_auth = Auth0DeviceAuthorization(auth0_domain, auth0_client_id, auth0_audience)
+    auth0_device_auth = Auth0DeviceAuthorization(AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_AUDIENCE)
 
     try:
         # Request a device code
@@ -28,19 +29,12 @@ def main():
         auth0_device_auth.display_user_instructions()
 
         # Poll the token endpoint to obtain an access token
-        access_token = auth0_device_auth.poll_token_endpoint()
+        access_token = await auth0_device_auth.poll_token_endpoint()
         print(f"Access Token: {access_token}")
-
-        # Optionally, refresh the access token using the refresh token
-        if auth0_device_auth.refresh_token:
-            new_access_token = auth0_device_auth.refresh_access_token(
-                auth0_device_auth.refresh_token
-            )
-            print(f"New Access Token: {new_access_token}")
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
--- nova/auth/authorization.py
+++ nova/auth/authorization.py
@@ -1,9 +1,9 @@
-import time
 import httpx
-from pydantic import BaseModel, Field, ValidationError
+import asyncio
+import pydantic
 
 
-class DeviceCodeInfo(BaseModel):
+class Auth0DeviceCodeInfo(pydantic.BaseModel):
     """
     Model to store device code information.
 
@@ -19,10 +19,10 @@ class DeviceCodeInfo(BaseModel):
     user_code: str
     verification_uri: str
     expires_in: int
-    interval: int = Field(default=5)
+    interval: int = pydantic.Field(default=5)
 
 
-class TokenInfo(BaseModel):
+class Auth0TokenInfo(pydantic.BaseModel):
     """
     Model to store token information.
 
@@ -35,7 +35,7 @@ class TokenInfo(BaseModel):
     refresh_token: str | None = None
 
 
-class Auth0Parameters(BaseModel):
+class Auth0Parameters(pydantic.BaseModel):
     """
     Model to store Auth0 parameters.
 
@@ -85,22 +85,23 @@ class Auth0DeviceAuthorization:
                 auth0_client_id=auth0_client_id,
                 auth0_audience=auth0_audience,
             )
-        except ValidationError as e:
+        except pydantic.ValidationError as e:
             raise ValueError(f"Error: The following parameters are not set correctly: {e}")
 
         self.auth0_domain = auth0_domain
         self.auth0_client_id = auth0_client_id
         self.auth0_audience = auth0_audience
         self.headers = {"content-type": "application/x-www-form-urlencoded"}
-        self.device_code_info: DeviceCodeInfo | None = None
+        self.device_code_info: Auth0DeviceCodeInfo | None = None
         self.interval = 5
+        self.attempts = 10
 
     def request_device_code(self):
         """
         Requests a device code from Auth0.
 
         Returns:
-            DeviceCodeInfo: The device code information.
+            Auth0DeviceCodeInfo: The device code information.
 
         Raises:
             Exception: If there is an error requesting the device code.
@@ -108,19 +109,28 @@ class Auth0DeviceAuthorization:
         device_code_url = f"https://{self.auth0_domain}/oauth/device/code"
         data = {
             "client_id": self.auth0_client_id,
-            "scope": "openid profile email offline_access",
+            "scope": "openid profile email",
             "audience": self.auth0_audience,
         }
 
         response = httpx.post(device_code_url, headers=self.headers, data=data)
         if response.status_code == 200:
-            self.device_code_info = DeviceCodeInfo(**response.json())
+            self.device_code_info = Auth0DeviceCodeInfo(**response.json())
             self.interval = self.device_code_info.interval
             return self.device_code_info
         else:
             raise Exception("Error requesting device code:", response.json())
 
-    def display_user_instructions(self):
+    def get_device_code_info(self) -> Auth0DeviceCodeInfo | None:
+        """
+        Returns the device code information.
+
+        Returns:
+            Auth0DeviceCodeInfo | None: The device code information.
+        """
+        return self.device_code_info
+
+    def display_user_instructions(self) -> None:
         """
         Displays instructions for the user to authenticate.
 
@@ -136,7 +146,7 @@ class Auth0DeviceAuthorization:
         else:
             raise Exception("Device code information is not available.")
 
-    def poll_token_endpoint(self):
+    async def poll_token_endpoint(self):
         """
         Polls the token endpoint to obtain an access token.
 
@@ -156,34 +166,32 @@ class Auth0DeviceAuthorization:
             "client_id": self.auth0_client_id,
         }
 
-        while True:
-            token_response = httpx.post(token_url, headers=self.headers, data=token_data)
-            if token_response.status_code == 200:
-                # If the response status is 200, it means the access token is successfully obtained.
-                token_info = TokenInfo(**token_response.json())
-                self.refresh_token = token_info.refresh_token
-                print("Access Token received:", token_info.access_token)
-                print("Refresh Token received:", self.refresh_token)
-                return token_info.access_token
-            elif token_response.status_code == 400:
-                # If the response status is 400, check the error type.
-                error = token_response.json().get("error")
-                if error == "authorization_pending":
-                    # If the error is 'authorization_pending', it means the user has not yet authorized.
-                    print("Waiting for user confirmation...")
-                    time.sleep(self.interval)
-                elif error == "slow_down":
-                    # If the error is 'slow_down', it means the server requests to slow down polling.
-                    print("Server requests to slow down polling. Increasing wait time.")
-                    self.interval += 5
-                    time.sleep(self.interval)
-            elif token_response.status_code == 403:
-                # If the response status is 403, it means the request is forbidden, wait and retry.
-                print("Waiting...")
-                time.sleep(self.interval)
-            else:
-                # For other status codes, raise an exception with the error details.
-                raise Exception("Error:", token_response.status_code, token_response.json())
+        async with httpx.AsyncClient() as client:
+            while self.attempts > 0:
+                self.attempts = self.attempts - 1
+                token_response = await client.post(token_url, headers=self.headers, data=token_data)
+                if token_response.status_code == 200:
+                    # If the response status is 200, it means the access token is successfully obtained.
+                    token_info = Auth0TokenInfo(**token_response.json())
+                    self.refresh_token = token_info.refresh_token
+                    return token_info.access_token
+                elif token_response.status_code == 400:
+                    # If the response status is 400, check the error type.
+                    error = token_response.json().get("error")
+                    if error == "authorization_pending":
+                        # If the error is 'authorization_pending', it means the user has not yet authorized.
+                        await asyncio.sleep(self.interval)
+                    elif error == "slow_down":
+                        # If the error is 'slow_down', it means the server requests to slow down polling.
+                        self.interval += 5
+                        await asyncio.sleep(self.interval)
+                elif token_response.status_code == 403:
+                    # If the response status is 403, it means the request is forbidden, wait and retry.
+                    await asyncio.sleep(self.interval)
+                else:
+                    # For other status codes, raise an exception with the error details.
+                    raise Exception("Error:", token_response.status_code, token_response.json())
+            raise Exception("Error: It was not able to authenticate. Please try again.")
 
     def refresh_access_token(self, refresh_token: str):
         """
@@ -209,9 +217,9 @@ class Auth0DeviceAuthorization:
         }
 
         response = httpx.post(token_url, headers=self.headers, data=token_data)
+
         if response.status_code == 200:
-            token_info = TokenInfo(**response.json())
-            print("New Access Token received:", token_info.access_token)
+            token_info = Auth0TokenInfo(**response.json())
             return token_info.access_token
         else:
             raise Exception("Error refreshing access token:", response.json())
