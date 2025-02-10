12466fc71d6bfb6dff1c9153bb2905ad2e79b4cf
André <andre.kuehnert@wandelbots.com>
2025-02-10 14:09:45 +0100

    feat(tests): enhance ApiGateway tests to validate host prefixing with parameterized inputs

--- tests/test_gateway.py
+++ tests/test_gateway.py
@@ -1,28 +1,50 @@
+import os
 import pytest
 
 from nova.gateway import ApiGateway
 
 
+@pytest.fixture
+def nova_api_env():
+    """Cleanup env vars after test"""
+    original_value = os.environ.get("NOVA_API")
+
+    def _set_nova_api(value: str):
+        os.environ["NOVA_API"] = value
+
+    yield _set_nova_api
+
+    if original_value is None:
+        os.environ.pop("NOVA_API", None)
+    else:
+        os.environ["NOVA_API"] = original_value
+
+
 @pytest.mark.asyncio
-async def test_api_gateway_host():
+@pytest.mark.parametrize(
+    "given,expected",
+    [
+        ("http://some-host.net", "http://some-host.net"),
+        ("https://some-host.net", "https://some-host.net"),
+        ("some-host.net", "http://some-host.net"),
+        ("someinstance.wandelbots.io", "https://someinstance.wandelbots.io"),
+        ("http://someinstance.wandelbots.io", "https://someinstance.wandelbots.io"),
+        ("https://someinstance.wandelbots.io", "https://someinstance.wandelbots.io"),
+        ("https://172.30.1.2", "https://172.30.1.2"),
+        ("http://172.30.1.2", "http://172.30.1.2"),
+        ("172.30.1.2", "http://172.30.1.2"),
+    ],
+)
+async def test_api_gateway_host(nova_api_env, given, expected):
     """
     NOVA_API env might not provide any prefix,
     thus ApiGateway should make sure to use the prefix depending on the environment.
 
     https://wandelbots.atlassian.net/browse/RPS-1208
     """
-    expected_host = "http://some-host.net"
-    gateway = ApiGateway(host=expected_host)
-    assert gateway._host == expected_host
+    gateway = ApiGateway(host=given)
+    assert gateway._host == expected
 
-    expected_host = "https://some-host.net"
-    gateway = ApiGateway(host=expected_host)
-    assert gateway._host == expected_host
-
-    expected_host = "http://some-host.net"
-    gateway = ApiGateway(host="some-host.net")
-    assert gateway._host == expected_host
-
-    expected_host = "https://someinstance.wandelbots.io"
-    gateway = ApiGateway(host="someinstance.wandelbots.io")
-    assert gateway._host == expected_host
+    nova_api_env(given)
+    gateway = ApiGateway()
+    assert gateway._host == expected
