b97481c6e99ebc2c55540c903746626ef328eb69
Dirk Sonnemann <dirk.sonnemann@wandelbots.com>
2025-02-15 13:56:05 +0100

    feat(RPS-1214): WIP fixing typing

--- nova/utils.py
+++ nova/utils.py
@@ -1,35 +1,37 @@
 import asyncio
-from typing import AsyncIterator, Callable, Generic, TypeVar
+from typing import AsyncIterable, AsyncIterator, Callable, Generic, TypeVar
 
+I = TypeVar("I")
+O = TypeVar("O")
 T = TypeVar("T")
 
 
-class StreamExtractor(Generic[T]):
+class StreamExtractor(Generic[I, O]):
     def __init__(
         self,
-        wrapped: Callable[[AsyncIterator[T]], AsyncIterator[T]],
-        stop_selector: Callable[[T], bool] | None = None,
+        wrapped: Callable[[AsyncIterator[I]], AsyncIterable[O]],
+        stop_selector: Callable[[I], bool] | None = None,
     ):
-        self._queue: asyncio.Queue[T | None] = asyncio.Queue()
+        self._in_queue: asyncio.Queue[I | None] = asyncio.Queue()
         self._wrapped = wrapped
         self._stop_selector = stop_selector or (lambda x: x is None)
 
-    def __call__(self, in_stream: AsyncIterator[T]) -> AsyncIterator[T]:
-        async def in_wrapper(in_stream_) -> AsyncIterator[T]:
+    def __call__(self, in_stream: AsyncIterable[I]) -> AsyncIterable[O]:
+        async def in_wrapper(in_stream_) -> AsyncIterator[I]:
             async for in_value in in_stream_:
                 if self._stop_selector(in_value):
-                    self._queue.put_nowait(None)
+                    self._in_queue.put_nowait(None)
                 else:
-                    self._queue.put_nowait(in_value)
+                    self._in_queue.put_nowait(in_value)
                 yield in_value
 
         return self._wrapped(in_wrapper(in_stream))
 
-    def __aiter__(self) -> AsyncIterator[T]:
+    def __aiter__(self) -> AsyncIterator[I]:
         return self
 
-    async def __anext__(self) -> T:
-        value = await self._queue.get()
+    async def __anext__(self) -> I:
+        value = await self._in_queue.get()
         if value is None:
             raise StopAsyncIteration
         return value
