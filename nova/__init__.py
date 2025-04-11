bdbfb20a264eef13c4b1feb1c27619c8de392798
cbiering <christoph.biering@wandelbots.com>
2025-04-11 09:36:16 +0200

    schema creation

--- nova/__init__.py
+++ nova/__init__.py
@@ -1,14 +1,17 @@
+import abc
+from functools import wraps
+
+import pydantic
+
 from nova import actions, api, types
 from nova.core.controller import Controller
 from nova.core.logging import logger
 from nova.core.motion_group import MotionGroup
-from nova.core.movement_controller import speed_up as speed_up_movement_controller
+from nova.core.movement_controller import \
+    speed_up as speed_up_movement_controller
 from nova.core.nova import Cell, Nova
 from nova.types import MotionSettings
 from nova.version import version
-from functools import wraps
-import pydantic
-import abc
 
 __version__ = version
 
@@ -17,7 +20,7 @@ class ProgramParameter(pydantic.BaseModel, abc.ABC):
     pass
 
 
-def program(parameter=ProgramParameter, name: str | None = None):
+def program(parameter: type[ProgramParameter], name: str | None = None):
     @wraps
     async def wrapped_function(func):
         if not callable(func):
@@ -26,7 +29,11 @@ def program(parameter=ProgramParameter, name: str | None = None):
         async with Nova() as nova:
             # TODO: should we pass nova completely
             # TODO: run function in Nova runtime
-            await func(nova_context=nova, arguments=parameter)
+            start_time = nova.time()
+            await func(nova_context=nova, kwargs=parameter.model_dump())
+            end_time = nova.time()
+            exection_time = end_time - start_time
+            logger.info(f"Execution time: {exection_time}")
     return wrapped_function
 
 
