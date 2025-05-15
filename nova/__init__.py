34dde3dc95e68394ecb7db5c58b9828119bef106
Stefan Wagner <stefan.wagner@wandelbots.com>
2025-05-15 13:52:16 +0200

    feat: expose combine_trajectories in the module exports (#161)

--- nova/__init__.py
+++ nova/__init__.py
@@ -2,7 +2,7 @@ from nova import actions, api, types
 from nova.cell.cell import Cell
 from nova.core.controller import Controller
 from nova.core.logging import logger
-from nova.core.motion_group import MotionGroup
+from nova.core.motion_group import MotionGroup, combine_trajectories
 from nova.core.movement_controller import speed_up as speed_up_movement_controller
 from nova.core.nova import Nova
 from nova.runtime.function import wrap as program
@@ -16,6 +16,7 @@ __all__ = [
     "Nova",
     "Cell",
     "MotionGroup",
+    "combine_trajectories",
     "Controller",
     "speed_up_movement_controller",
     "api",
