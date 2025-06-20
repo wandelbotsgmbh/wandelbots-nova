b04c96cb76bb32d94b09b2cf0f5c4da2e14ff9ff
mahsumdemirwb <mahsum.demir@wandelbots.com>
2025-06-20 15:41:53 +0200

    test

--- nova_app_template/__init__.py
+++ nova_app_template/__init__.py
@@ -1,13 +1,12 @@
-from .decorators import program
-from .processors import processors_container
-from .container import parse_model_from_args, create_container
 from .api import create_nova_api_app
+from .cli import parse_model_from_args
+from .container import create_container
+from .decorators import program
 from .store.models import BaseProgramModel
 
 __all__ = [
     "program",
     "parse_model_from_args",
-    "processors_container",
     "create_nova_api_app",
     "create_container",
     "BaseProgramModel",
