2cda725070fa3df94aa1ff4871ac444b4e5e5d72
mahsumdemirwb <mahsum.demir@wandelbots.com>
2025-07-21 15:55:39 +0200

    delete file and add program

--- nova/program/__init__.py
+++ nova/program/__init__.py
@@ -2,4 +2,4 @@ from nova.program.function import Program, ProgramPreconditions, program
 from nova.program.runner import ProgramRunner
 from nova.program.store import ProgramStore
 
-__all__ = ["ProgramRunner", "program", "ProgramPreconditions", "ProgramStore"]
+__all__ = ["ProgramRunner", "program", "ProgramPreconditions", "ProgramStore", "Program"]
