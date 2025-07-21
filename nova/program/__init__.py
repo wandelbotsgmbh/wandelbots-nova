93b1ee9efadc9484508b546523cd0765e264f922
mahsumdemirwb <mahsum.demir@wandelbots.com>
2025-07-21 09:14:29 +0200

    run uv sync and ruff

--- nova/program/__init__.py
+++ nova/program/__init__.py
@@ -6,9 +6,10 @@ from nova.program.runner import ProgramRunner
 # when I do store = ProgramStore() the __orig_class__ is not available in the __init__
 # my reseach say's python doesn't capture the type argument when I do this
 
-# TODO: change the Program with wandelbots_api_client.v2.models.Program 
+
+# TODO: change the Program with wandelbots_api_client.v2.models.Program
 class ProgramStore(KeyValueStore[Program]):
-    def __init__(self, nats_bucket_name, nats_client_config = None, nats_kv_config = None):
+    def __init__(self, nats_bucket_name, nats_client_config=None, nats_kv_config=None):
         super().__init__(Program, nats_bucket_name, nats_client_config, nats_kv_config)
 
 
