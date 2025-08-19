f1f38a192b5f69a3bd4f5cefc869795137b17aa5
Christoph Biering <1353438+biering@users.noreply.github.com>
2025-08-19 10:27:14 +0200

    feat(NDX-227): added vscode extension with basic setup to wandelbots-nova (#261)
    Co-authored-by: Christoph Biering <christoph.biering@wandelbots.com>

---
+++ vscode-ext/README.md
@@ -0,0 +1,23 @@
+# wandelbots-nova README
+
+The Wandelbots NOVA extension for VSCode.
+
+## Local Development
+
+1. Install the dependencies:
+
+```bash
+pnpm install
+```
+
+2. Build & package the extension:
+
+```bash
+pnpm run package
+```
+
+3. Install the extension in VSCode via VSIX. Search for the command `Extensions: Install from VSIX...` and select the created file.
+
+4. Reload the extension with the command `Developer: Reload Window`.
+
+You should now see the extension in the Extensions view & the Wandelbots logo in the activity bar.
---
+++ vscode-ext/src/consts.ts
@@ -0,0 +1,13 @@
+export const VIEWER_ID = 'wandelbots-nova-viewer'
+export const EXPLORER_ID = 'wandelbots-nova-explorer'
+export const COMMAND_PREFIX = 'wandelbots-nova'
+export const COMMAND_RUN_NOVA_PROGRAM = `${COMMAND_PREFIX}.runNovaProgram`
+export const COMMAND_DEBUG_NOVA_PROGRAM = `${COMMAND_PREFIX}.debugNovaProgram`
+export const COMMAND_REFRESH_CODE_LENS = `${COMMAND_PREFIX}.refreshCodeLens`
+export const COMMAND_OPEN_NOVA_VIEWER = `${COMMAND_PREFIX}.open`
+export const COMMAND_REFRESH_NOVA_VIEWER = `${COMMAND_PREFIX}.refresh`
+export const COMMAND_SHOW_APP = `${COMMAND_PREFIX}.showApp`
+export const COMMAND_SHOW_PANEL = `${COMMAND_PREFIX}.showPanel`
+
+export const SETTINGS_NOVA_API = 'novaApi' as const
+export const SETTINGS_RERUN_ADDRESS = 'rerunAddress' as const
