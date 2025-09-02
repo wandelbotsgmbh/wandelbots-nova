8355094b3d28f78c778309843d463a13079124e4
Christoph Biering <christoph.biering@wandelbots.com>
2025-09-02 16:49:11 +0200

    wip

--- vscode-ext/app/src/pages/JoggingPanel.tsx
+++ vscode-ext/app/src/pages/JoggingPanel.tsx
@@ -1,22 +1,41 @@
 import { JoggingPanel as LibraryJoggingPanel } from '@wandelbots/wandelbots-js-react-components'
 import React, { useMemo } from 'react'
+import { NovaClient } from '@wandelbots/nova-js/v1'
 
 function getConfig() {
   const ls = typeof window !== 'undefined' ? window.localStorage : undefined
-  const novaApi = ls?.getItem('wandelbots-nova-viewer.novaApi') || ''
-  const cellId = ls?.getItem('wandelbots-nova-viewer.cellId') || 'cell'
+  const injected =
+    (typeof window !== 'undefined' && (window as any).__NOVA_CONFIG__) || {}
+  const novaApi =
+    injected.novaApi || ls?.getItem('wandelbots-nova-viewer.novaApi') || ''
+  const cellId =
+    injected.cellId || ls?.getItem('wandelbots-nova-viewer.cellId') || 'cell'
+  const accessToken =
+    injected.accessToken || ls?.getItem('wandelbots-nova-viewer.accessToken') || ''
   const motionGroupId =
     ls?.getItem('wandelbots-nova-viewer.motionGroupId') || '0@robot'
-  return { novaApi, cellId, motionGroupId }
+  return { novaApi, cellId, accessToken, motionGroupId }
 }
 
 export default function JoggingPanel() {
-  const { novaApi, motionGroupId } = useMemo(() => getConfig(), [])
+  const { novaApi, cellId, accessToken, motionGroupId } = useMemo(
+    () => getConfig(),
+    [],
+  )
+
+  const novaClient = new NovaClient({
+    instanceUrl: novaApi || 'http://localhost',
+    cellId,
+    accessToken,
+  })
 
   return (
-    <LibraryJoggingPanel
-      nova={novaApi || 'http://localhost:8000'}
-      motionGroupId={motionGroupId}
-    />
+    <div>
+      <div>{novaApi}</div>
+      <div>{cellId}</div>
+      <div>{accessToken}</div>
+      <div>{motionGroupId}</div>
+      <LibraryJoggingPanel nova={novaClient} motionGroupId={motionGroupId} />
+    </div>
   )
 }
