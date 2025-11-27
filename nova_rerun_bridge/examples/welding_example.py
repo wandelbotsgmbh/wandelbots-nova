785a5023904f234a9c0fd4727d75540a7e8dd535
Christoph Biering <christoph.biering@wandelbots.com>
2025-11-27 10:37:57 +0100

    fix

--- nova_rerun_bridge/examples/welding_example.py
+++ nova_rerun_bridge/examples/welding_example.py
@@ -180,7 +180,7 @@ async def calculate_seam_poses(mesh_pose: api.models.Pose) -> tuple[Pose, Pose, 
             virtual_controller(
                 name="ur10",
                 manufacturer=api.models.Manufacturer.UNIVERSALROBOTS,
-                type=api.models.VirtualControllerTypes.UNIVERSALROBOTS_MINUS_UR10E,
+                type=api.models.VirtualControllerTypes.UNIVERSALROBOTS_UR10E,
             )
         ],
         cleanup_controllers=False,
